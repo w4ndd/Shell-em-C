d6ed2dcadd12e7e8e202dbcdc8dc52eed9a63270
André Barros <andre.barros1337@gmail.com>
2024-04-24 19:26:38 -0300

    Função lsh_launch
    Adiciona função lsh_launch ao interpretador de shell, que executa comandos e espera até a conclusão do processo filho

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 //Function to read
 #include <stdlib.h>
+
 #define LSH_RL_BUFSIZE 1024
 
 char *lsh_read_size(void)
@@ -78,7 +79,36 @@ char **lsh_split_line(char *line)
     tokens[position] = NULL;
     return tokens;
 }
+// launch program
+#include <sys/types.h>
+#include <sys/wait.h>
 
+int lsh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        //Child process
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("lsh");
+    } else {
+        // Parent process
+        do
+        {
+           wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        
+    }
+
+    return 1;    
+}
 
 void lsh_loop(void)
 {
